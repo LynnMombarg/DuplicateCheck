a8fc349b32b0cb15388c9a13b26f5b9088d374ca
Silke Bertisen <sbertisen@hotmail.nl>
2023-04-17 16:26:20 +0200

    start testpagina

--- vue_ui/src/App.vue
+++ vue_ui/src/App.vue
@@ -1,47 +1,76 @@
 <script setup>
+import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+let user =  ref('Salesforce user')
+let showWelcome = true
+let message = ref('Press the "Change message button" to change the message')
+
+function toggleWelcomeComponent(){
+  this.showWelcome = false
+}
+function changeMessage(){
+  message.value = 'The button was pressed'
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <h1> Dashboard </h1>
+  <p> logged in as {{ user }} </p>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <h1> The component for doing a test API call: </h1>
+
+  <button @click="toggleWelcomeComponent"> Toggle welcome component </button>
+  <button @click="this.showWelcome=false"> false  </button>
+  <button @click="changeMessage"> Change message </button>
+
+  <div v-if="showWelcome"> <TheWelcome /> </div>
+  <div v-else> Hidden </div>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<!--<template>-->
+<!--  <header>-->
+<!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<!--    <div class="wrapper">-->
+<!--      <HelloWorld msg="You did it!" />-->
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+<!--    </div>-->
+<!--  </header>-->
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+<!--  <main>-->
+<!--    <TheWelcome />-->
+<!--  </main>-->
+<!--</template>-->
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<!--<style scoped>-->
+<!--header {-->
+<!--  line-height: 1.5;-->
+<!--}-->
+
+<!--.logo {-->
+<!--  display: block;-->
+<!--  margin: 0 auto 2rem;-->
+<!--}-->
+
+<!--@media (min-width: 1024px) {-->
+<!--  header {-->
+<!--    display: flex;-->
+<!--    place-items: center;-->
+<!--    padding-right: calc(var(&#45;&#45;section-gap) / 2);-->
+<!--  }-->
+
+<!--  .logo {-->
+<!--    margin: 0 2rem 0 0;-->
+<!--  }-->
+
+<!--  header .wrapper {-->
+<!--    display: flex;-->
+<!--    place-items: flex-start;-->
+<!--    flex-wrap: wrap;-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
--- vue_ui/src/main.js
+++ vue_ui/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+// import the corret styles here (tailwind)
+//import './assets/main.css'
 
 createApp(App).mount('#app')
