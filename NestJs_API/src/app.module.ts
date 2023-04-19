cc13db6fbc58a3fafe7fe4e4d59bef76d648fc80
Roward Dorrestijn <RED.Dorrestijn@student.han.nl>
2023-04-19 14:30:46 +0200

    Small fix

--- NestJs_API/src/app.module.ts
+++ NestJs_API/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {Message, MessageSchema } from './dto/message.schema';
+import { ItemModule } from './item/item.module';
+import { LoginModule } from './login/login.module';
 import { PythonModule } from './python/python.module';
 
 @Module({
-  imports: [PythonModule],
+  imports: [
+      MongooseModule.forRoot(
+      'mongodb+srv://pascal:ditIsEenLeukProject23@mar.jbpp1g5.mongodb.net/DuplicateCheck',
+  ),
+    MongooseModule.forFeature([{name: Message.name, schema: MessageSchema}]),
+    ItemModule,
+    LoginModule,
+      PythonModule],
   controllers: [AppController],
   providers: [AppService],
 })
