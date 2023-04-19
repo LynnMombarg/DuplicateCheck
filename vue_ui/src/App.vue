3eeae1bdc8925c09498b744efb67da7e01955483
Diederik Lensink <diederik1905@gmail.com>
2023-04-19 11:05:39 +0200

    Merged <style scoped> and <style> in App.vue

--- vue_ui/src/App.vue
+++ vue_ui/src/App.vue
@@ -16,36 +16,33 @@ import SignInFooter from "@/components/SignInFooter.vue";
   </main>
 </template>
 
-<style scoped>
+<style>
 header {
-  line-height: 1.5;
+    line-height: 1.5;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+    display: block;
+    margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    .logo {
+        margin: 0 2rem 0 0;
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
 }
-</style>
-
-<style>
 #app {
   font-family: "Ubuntu", sans-serif;
   -webkit-font-smoothing: antialiased;
