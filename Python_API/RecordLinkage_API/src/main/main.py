858d83d499e31171d5bb5d7d98f7e3efdfb4605e
Lynn Mombarg <L.Mombarg1@student.han.nl>
2023-04-19 13:55:06 +0200

    main.py edited online with Bitbucket

--- Python_API/RecordLinkage_API/src/main/main.py
+++ Python_API/RecordLinkage_API/src/main/main.py
@@ -2,18 +2,21 @@ from fastapi.responses import JSONResponse
 from fastapi import FastAPI, status
 from recordLinkage.RecordLinkage import RecordLinkageModel
 
-app = FastAPI()
-model = RecordLinkageModel()
+class main:
+  global app
+  global model
+  app = FastAPI()
+  model = RecordLinkageModel()
 
-# Easily made endpoint to test simple get requests
-@app.get('/get-candidates')
-async def duplicateCandidates():
-    return JSONResponse('{ "duplicates": { "Name": "Jan" }, { "Name": "Piet" } }')
+  # Easily made endpoint to test simple get requests
+  @app.get('/get-candidates')
+  async def duplicateCandidates():
+      return JSONResponse('{ "duplicates": { "Name": "Jan" }, { "Name": "Piet" } }')
 
-# Post request to send json datasets to train the model
-# Expected form of json:
-# {"recordset1": [{"columns":}, {"columns":}], "recordset2": [{"columns":}, {"columns":}], "golden_matches_index": [{"index1": , "index2": }]}
-@app.post('/train-model', status_code=status.HTTP_201_CREATED)
-async def duplicateCandidates(json_dataframe : dict):
-    model.trainModel(json_dataframe)
-    return model.nrOfTrainings
+  # Post request to send json datasets to train the model
+  # Expected form of json:
+  # {"recordset1": [{"columns":}, {"columns":}], "recordset2": [{"columns":}, {"columns":}], "golden_matches_index": [{"index1": , "index2": }]}
+  @app.post('/train-model', status_code=status.HTTP_201_CREATED)
+  async def duplicateCandidates(json_dataframe : dict):
+      model.trainModel(json_dataframe)
+      return model.nrOfTrainings
