70bd017132bf28cc63f7aed27da12d95f1a3d727
Lynn Mombarg <L.Mombarg1@student.han.nl>
2023-04-19 15:05:48 +0200

    main.py edited online with Bitbucket

--- Python_API/RecordLinkage_API/src/main/main.py
+++ Python_API/RecordLinkage_API/src/main/main.py
@@ -1,6 +1,6 @@
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI, status
-from recordLinkage.RecordLinkage import RecordLinkageModel
+from Python_API.RecordLinkage_API.src.main.recordLinkage.RecordLinkage import RecordLinkageModel
 
 class main:
   global app
