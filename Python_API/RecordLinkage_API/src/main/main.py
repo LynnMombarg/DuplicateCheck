a4051b2ad30a33ea69d51d5f3693f7f51833858f
Lynn <l.mombarg1@student.han.nl>
2023-04-19 10:56:19 +0200

    object oriented

--- Python_API/RecordLinkage_API/src/main/main.py
+++ Python_API/RecordLinkage_API/src/main/main.py
@@ -1,8 +1,10 @@
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI
-app = FastAPI()
 
-@app.get('/get-candidates')
-async def duplicateCandidates():
-    return JSONResponse('{ "record1": { "Name": "Jan" }, "record2": { "Name": "Piet" } }')
+class main:
+    global app
+    app = FastAPI()
 
+    @app.get('/get-candidates')
+    async def duplicateCandidates():
+        return JSONResponse('{ "duplicates": { "Name": "Jan" }, { "Name": "Piet" } }')
