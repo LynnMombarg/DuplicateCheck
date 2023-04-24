95159fc36bb11622099966794c8966107ec041f3
Silke Bertisen <sbertisen@hotmail.nl>
2023-04-24 15:04:48 +0200

    Dashboard expanded

--- vue_ui/src/components/Dashboard.vue
+++ vue_ui/src/components/Dashboard.vue
@@ -1,4 +1,6 @@
 <template>
+
+
   <ul role="list" class="divide-y divide-gray-100">
     <li v-for="model in models" :key="model.Information" class="flex py-5">
       <div class="bg-white shadow w-full">
@@ -8,14 +10,14 @@
               <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page"> {{model.name}}</a>
             </div>
             <div class="min-w-0 flex-auto">
-              <p class="text-sm  leading-6 text-gray-900">{{ model.Information}}</p>
+              <p class="text-sm  leading-6 text-gray-900">  {{model.Information}}</p>
             </div>
           </div>
+          <p class="text-xs leading-5 text-gray-500"> created on: {{model.createdOn}}</p>
           <div class="hidden sm:flex sm:flex-col sm:items-end">
-            <p class="text-xs leading-5 text-gray-500"> {{model.createdOn}}</p>
+            <model-options-dropdown/>
           </div>
         </div>
-
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex justify-middle">
           <p class="text-sm leading-6 text-gray-900">{{ model.details}}</p>
         </div>
@@ -23,10 +25,12 @@
       </div>
     </li>
   </ul>
+
 </template>
 
-
 <script setup>
+import ModelOptionsDropdown from "@/components/ModelOptionsDropdown.vue";
+
 const models = [
   {
     name: 'model1',
@@ -42,3 +46,15 @@ const models = [
   }
 ]
 </script>
+
+
+
+<style>
+.dropdown:focus-within .dropdown-menu {
+  opacity:1;
+  transform: translate(0) scale(1);
+  visibility: visible;
+}
+</style>
+
+
