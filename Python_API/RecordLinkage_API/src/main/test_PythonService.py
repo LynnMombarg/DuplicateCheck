e14c4ccbc48ceb7b692373f163e6136e9788a13e
Lynn <l.mombarg1@student.han.nl>
2023-04-26 16:34:36 +0200

    - pkl

--- Python_API/RecordLinkage_API/src/main/test_PythonService.py
+++ Python_API/RecordLinkage_API/src/main/test_PythonService.py
@@ -19,7 +19,7 @@ class test_PythonService(TestCase):
     
     def test_createModel(self):
         try:
-            self.sut.createModel('test.pkl')
+            self.sut.createModel('test')
         except:
             self.fail()
         
@@ -29,16 +29,17 @@ class test_PythonService(TestCase):
         jsonObject = json.loads(jsonString)
         
         with self.assertRaises(FileNotFoundError):
-            self.sut.trainModel('notExisting.pkl', jsonObject)
+            self.sut.trainModel('notExisting', jsonObject)
             
 
     def test_trainExistingModel(self):
         try:
-            self.sut.createModel('test.pkl')
+            self.sut.createModel('test')
             jsonString = '{"recordset1": [{"name": "Piet", "lastname": "Janssen", "age": "18", "country": "NL"}, {"name": "Jan", "lastname": "Pietersen", "age": "19", "country": "BE"}], "recordset2": [{"name": "Henk", "lastname": "van Dijk", "age": "20", "country": "DE"}, {"name": "Piet", "lastname": "Jansen", "age": "18", "country": "NL"}], "golden_matches_index": [{"index1": 0, "index2": 1}]}'
             jsonObject = json.loads(jsonString)
-            self.sut.trainModel('test.pkl', jsonObject)
+            self.sut.trainModel('test', jsonObject)
         except:
             self.fail()
+        
             
 unittest.main()
