72f59c533604aef77afe576dc3ba668794147d25
Roward Dorrestijn <RED.Dorrestijn@student.han.nl>
2023-05-10 09:28:35 +0200

    Small change

--- NestJs_API/src/model/model.DAO.ts
+++ NestJs_API/src/model/model.DAO.ts
@@ -1,16 +1,30 @@
+// Authors: Marloes
+// Jira-task: 107 - Models toevoegen aan database
+// Sprint: 2
+// Last modified: 26-04-2023
+
+import { ModelDTO } from './dto/model.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from './model.schema';
 import mongoose from 'mongoose';
 import { Injectable } from '@nestjs/common';
-import { DisplayDTO } from './display-model.DTO';
 
 @Injectable()
 export class ModelDAO {
-  constructor(
-    @InjectModel(Model.name) private modelModel: mongoose.Model<Model>,
-  ) {}
+  constructor(@InjectModel(Model.name) private model: mongoose.Model<Model>) {}
 
-  async getAllModels(userId: string): Promise<DisplayDTO[]> {
-    return this.modelModel.find({ userId: userId }, { userId: 0, _id: 0}).exec();
+  createModel(model: ModelDTO) {
+    const createdModel = new this.model(model);
+    createdModel.save();
+  }
+
+  async getAllModels(): Promise<ModelDTO[]> {
+    return this.model.find().exec();
+  }
+
+  async deleteModel(modelId: string): Promise<void> {
+    await this.model.deleteOne({
+      modelId: modelId,
+    });
   }
 }
