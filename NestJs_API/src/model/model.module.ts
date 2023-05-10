1fd48f74fff5335ad53f9cb2b3d4fc80c1bfa53b
DDRK <diederik1905@gmail.com>
2023-05-10 11:54:45 +0200

    Changed model module to use the auth flow

--- NestJs_API/src/model/model.module.ts
+++ NestJs_API/src/model/model.module.ts
@@ -9,14 +9,14 @@ import { ModelController } from './model.controller';
 import { ModelService } from './model.service';
 import { ModelDAO } from './model.dao';
 import { Model, ModelSchema } from './model.schema';
-import { LoginModule } from '../login/login.module';
 import { PythonModule } from '../python/python.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Model.name, schema: ModelSchema }]),
     PythonModule,
-    LoginModule,
+    AuthModule,
   ],
   controllers: [ModelController],
   providers: [ModelService, ModelDAO],
