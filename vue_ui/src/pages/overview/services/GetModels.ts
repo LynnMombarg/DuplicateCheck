4f5c47059d4ca46f48d35ffb7412b7c326c7fd18
Roward Dorrestijn <RED.Dorrestijn@student.han.nl>
2023-05-11 14:40:50 +0200

    Fixed live updates of adding and deleting models

--- vue_ui/src/pages/overview/services/GetModels.ts
+++ vue_ui/src/pages/overview/services/GetModels.ts
@@ -3,7 +3,7 @@
 // Sprint: 2
 // Last modified: 08-05-2023 
 
-export async function getData() {
+export async function getModels() {
   const response = await fetch("http://localhost:8001/model/models", {
     method: "GET",
     headers: {
@@ -14,8 +14,3 @@ export async function getData() {
   const data = await response.json();
   return data;
 }
-
-
-// To retrieve models for example:
-// let models = await getData();
-// console.log(models[0]["modelName"]);
