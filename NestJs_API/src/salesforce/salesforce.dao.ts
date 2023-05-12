84ffd39f8ec08473ca4cd4c2b750b90065e4c118
Roward Dorrestijn <RED.Dorrestijn@student.han.nl>
2023-05-12 14:17:59 +0200

    Update

--- NestJs_API/src/salesforce/salesforce.dao.ts
+++ NestJs_API/src/salesforce/salesforce.dao.ts
@@ -7,20 +7,19 @@ import { Injectable } from '@nestjs/common';
 import { AuthDTO } from 'src/auth/auth.dto';
 import { JobDTO } from 'src/model/dto/job-model.dto';
 
-const jsforce = require('jsforce');
-const oauth2 = new jsforce.OAuth2({
-  loginUrl: 'https://login.salesforce.com',
-  clientId: process.env.SF_CLIENT_ID,
-  clientSecret: process.env.SF_CLIENT_SECRET,
-  redirectUri: process.env.BASE_URL + '/auth/callback',
-});
-
 @Injectable()
 export class SalesforceDAO {
+	resultSet = [];
+  jsforce = require('jsforce');
+  oauth2 = new this.jsforce.OAuth2({
+    loginUrl: 'https://login.salesforce.com',
+    clientId: process.env.SF_CLIENT_ID,
+    clientSecret: process.env.SF_CLIENT_SECRET,
+    redirectUri: process.env.BASE_URL + '/auth/callback',
+  });
   async getJobs(tableId: String, tokens: AuthDTO): Promise<JobDTO[]> {
-    let resultSet = [];
-    const conn = new jsforce.Connection({
-      oauth2: oauth2,
+    const conn = new this.jsforce.Connection({
+      oauth2: this.oauth2,
       instanceUrl: process.env.SF_INSTANCE_URL,
       accessToken: tokens.getAccessToken(),
       refreshToken: tokens.getRefreshToken(),
@@ -32,17 +31,26 @@ export class SalesforceDAO {
         this.authService.updateToken(accessToken);
       }.bind(this),
     );
-
-    conn.query('SELECT id, name,  FROM dupcheck__dcJob__c WHERE dupcheck__SourceObject__c = "' + tableId + '"', function (err, result) {
-      if (err) {
-        return console.error(err);
-      }
-      for (var i = 0; i < result.records.length; i++) {
-        let jobName = result.records[i]["Name"];
-        let jobId = result.records[i]["Id"];
-        resultSet.push(new JobDTO(jobName, jobId));
-      }
-    });
-    return resultSet;
+    conn.query(
+      "SELECT id, name FROM dupcheck__dcJob__c WHERE dupcheck__SourceObject__c = '" +
+        tableId +
+        "'",
+      function (err, result) {
+        if (err) {
+          return console.error(err);
+        }
+		this.resultSet = [];
+		console.log("test");
+		// console.log(result.records);
+        for (var i = 0; i < result.records.length; i++) {
+          let jobName = result.records[i]['Name'];
+          let jobId = result.records[i]['Id'];
+		  console.log(jobId + ' ' + jobName);
+          this.resultSet.push(new JobDTO(jobName, jobId));
+		  console.log(this.resultSet);
+        }
+      },
+	);
+    return this.resultSet;
   }
 }
