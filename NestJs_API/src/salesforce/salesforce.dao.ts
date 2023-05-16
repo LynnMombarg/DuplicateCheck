0e449f2ca2f88df524678442923c5e692294f7ab
Roward Dorrestijn <RED.Dorrestijn@student.han.nl>
2023-05-16 16:35:06 +0200

    Added queries

--- NestJs_API/src/salesforce/salesforce.dao.ts
+++ NestJs_API/src/salesforce/salesforce.dao.ts
@@ -7,6 +7,7 @@ import {
   Injectable,
   NotFoundException,
   UnauthorizedException,
+  BadRequestException,
 } from '@nestjs/common';
 import { AuthDTO } from 'src/auth/auth.dto';
 import { AuthService } from '../auth/auth.service';
@@ -86,9 +87,16 @@ export class SalesforceDAO {
         break;
       default:
         throw new BadRequestException();
-        break;
     }
-    const resultSet: DatasetDTO[] = [];
+    const resultSet: string[] = [];
+
+    const sourceIndexes = await this.getSourceIndexes(jobId, tokens);
+    const matchIndexes = await this.getMatchIndexes(jobId, tokens);
+    const datasetA = await this.getMatchRecords(columns, matchIndexes, tokens);
+  }
+
+  async getSourceIndexes(jobId: string, tokens: AuthDTO): Promise<string[]> {
+    const resultSet: string[] = [];
 
     await new Promise((resolve, reject) => {
       const conn = new this.jsforce.Connection({
@@ -105,20 +113,99 @@ export class SalesforceDAO {
         }.bind(this),
       );
       conn.query(
-        "SELECT id, " + columns + " FROM " + tableName + " WHERE EXISTS = '" +
+        "SELECT dupcheck__SourceObject__c, dupcheck__Group__c FROM dupcheck__dc3Duplicate__c D WHERE dupcheck__dcGroup__c IN (SELECT G.Id FROM dupcheck__dcGroup__c G WHERE G.dupcheck__dcJob__c = '" +
           jobId +
-          "'",
-
-          //SELECT dupcheck__SourceObject__c, dupcheck__Group__c FROM dupcheck__dc3Duplicate__c D WHERE dupcheck__Group__c IN (SELECT G.Id FROM dupcheck__dcGroup__c G WHERE G.dupcheck__dcJob__c = 'a0C7Q00000Cz26UUAR')
+          "')",
         (err, result) => {
           if (err) {
             console.log(err);
             reject(new UnauthorizedException());
           } else {
             for (let i = 0; i < result.records.length; i++) {
-              const jobName = result.records[i]['Name'];
-              const jobId = result.records[i]['Id'];
-              resultSet.push(new JobDTO(jobName, jobId));
+              const index = result.records[i]['dupcheck__SourceObject__c'];
+              resultSet.push(index);
+            }
+            resolve(resultSet);
+          }
+        },
+      );
+    });
+    return resultSet;
+  }
+
+  async getMatchIndexes(jobId: string, tokens: AuthDTO): Promise<string> {
+    let resultSet = "";
+
+    await new Promise((resolve, reject) => {
+      const conn = new this.jsforce.Connection({
+        oauth2: this.oauth2,
+        instanceUrl: process.env.SF_INSTANCE_URL,
+        accessToken: tokens.getAccessToken(),
+        refreshToken: tokens.getRefreshToken(),
+      });
+      conn.on(
+        'refresh',
+        function (accessToken, res) {
+          console.log('refreshed token: ' + accessToken);
+          this.authService.updateToken(accessToken);
+        }.bind(this),
+      );
+      conn.query(
+        "SELECT dupcheck__MatchObject__c, dupcheck__Group__c FROM dupcheck__dc3Duplicate__c D WHERE dupcheck__dcGroup__c IN (SELECT G.Id FROM dupcheck__dcGroup__c G WHERE G.dupcheck__dcJob__c = '" +
+          jobId +
+          "')",
+        (err, result) => {
+          if (err) {
+            console.log(err);
+            reject(new UnauthorizedException());
+          } else {
+            for (let i = 0; i < result.records.length; i++) {
+              const index = result.records[i]['dupcheck__MatchObject__c'];
+              resultSet += ("'" + index + "',");
+            }
+            resolve(resultSet);
+          }
+        },
+      );
+    });
+    resultSet = resultSet.slice(0, -1);
+    return resultSet;
+  }
+
+  async getMatchRecords(
+    columns: string,
+    tableName: string,
+    matchIndexes: string,
+    tokens: AuthDTO,
+  ): Promise<string[]> {
+    const resultSet: RecordDTO[] = [];
+
+    await new Promise((resolve, reject) => {
+      const conn = new this.jsforce.Connection({
+        oauth2: this.oauth2,
+        instanceUrl: process.env.SF_INSTANCE_URL,
+        accessToken: tokens.getAccessToken(),
+        refreshToken: tokens.getRefreshToken(),
+      });
+      conn.on(
+        'refresh',
+        function (accessToken, res) {
+          console.log('refreshed token: ' + accessToken);
+          this.authService.updateToken(accessToken);
+        }.bind(this),
+      );
+      conn.query(
+        "SELECT " + columns + " FROM " + tableName + " WHERE Id IN (" +
+          matchIndexes +
+          ")",
+        (err, result) => {
+          if (err) {
+            console.log(err);
+            reject(new UnauthorizedException());
+          } else {
+            for (let i = 0; i < result.records.length; i++) {
+              const index = result.records[i]['dupcheck__MatchObject__c'];
+              resultSet.push(index);
             }
             resolve(resultSet);
           }
