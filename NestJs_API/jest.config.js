d2af25f1e72eb95be59a3990f8f191642561edf6
DDRK <diederik1905@gmail.com>
2023-05-22 12:34:37 +0200

    Delete jest.config.js

--- NestJs_API/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverage: true,
-  coverageReporters: ['lcov', 'text'],
-  coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/node_modules/', '/src/**/*.spec.ts'],
-};
