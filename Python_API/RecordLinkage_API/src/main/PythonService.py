f99fa54db3b369a44ab603d6f88b3cb06ad24501
MarloesStb <MEM.Steenbergen@student.han.nl>
2023-05-24 10:53:40 +0200

    merge commit

--- Python_API/RecordLinkage_API/src/main/PythonService.py
+++ Python_API/RecordLinkage_API/src/main/PythonService.py
@@ -8,39 +8,44 @@ Last modified: 16-05-2023
 import os
 import sys
 import json
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from .RecordLinkageModel import RecordLinkageModel
 import pickle
 
+
 class PythonService:
-    
+
     def create_model(self, model_id):
         model = RecordLinkageModel()
-        filehandler = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..' , 'pickles', model_id + '.pkl'), 'wb')
+        filehandler = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'pickles', model_id + '.pkl'),
+                           'wb')
         pickle.dump(model, filehandler)
-        
+
     def load_model(self, model_id):
         model: RecordLinkageModel
-        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..' , 'pickles', model_id + '.pkl'), 'rb') as file:
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'pickles', model_id + '.pkl'),
+                  'rb') as file:
             model = pickle.load(file)
             if not model:
                 raise FileNotFoundError('Model not found')
-            else :
+            else:
                 return model
-    
+
     def save_model(self, model_id, model):
-        filehandler = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..' , 'pickles', model_id + '.pkl'), 'wb')
+        filehandler = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'pickles', model_id + '.pkl'),
+                           'wb')
         pickle.dump(model, filehandler)
-        
+
     def train_model(self, model_id, json_dataframe):
         model = self.load_model(model_id)
         model.train_model(json_dataframe)
         self.save_model(model_id, model)
-        
+
     def delete_model(self, model_id):
-        os.remove(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..' , 'pickles', model_id + '.pkl'))
-    
+        os.remove(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'pickles', model_id + '.pkl'))
+
     def execute_model(self, model_id, json_dataframe):
         model = self.load_model(model_id)
         matches = model.execute_model(json_dataframe)
