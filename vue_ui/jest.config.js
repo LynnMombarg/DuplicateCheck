a1a184ded31b94c083f3651624b63175c741554f
Silke Bertisen <sbertisen@hotmail.nl>
2023-05-24 12:53:13 +0200

    ts-jest toegevoegd aan config

--- vue_ui/jest.config.js
+++ vue_ui/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     transform: {
       "^.+\\.jsx?$": "babel-jest",
       '^.+\\.vue$': '@vue/vue3-jest',
+      '^.+\\.ts?$': 'ts-jest',
     },
     testEnvironment: "jsdom",
     testEnvironmentOptions: {
