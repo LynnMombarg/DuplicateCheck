a0a29bde8ac5a5c0ef8887bc07c64bb953b14295
DDRK <diederik1905@gmail.com>
2023-05-30 15:59:23 +0200

    Working TrainingPage test

--- vue_ui/__tests__/pages/training/TrainingPage.spec.js
+++ vue_ui/__tests__/pages/training/TrainingPage.spec.js
@@ -1,14 +1,40 @@
 // Authors: Diederik
 // Jira-task: 152
-// Sprint: 3
-// Last modified: 24-05-2023
+// Sprint: 4
+// Last modified: 30-05-2023
 
-import TrainingPage from '../../../src/pages/training/TrainingPage.vue'
-import { shallowMount } from '@vue/test-utils';
+import TrainingPage from "@/pages/training/TrainingPage.vue";
+import { shallowMount } from "@vue/test-utils";
+import * as getJobsModule from "@/pages/training/services/GetJobs";
+
 
 describe("TrainingPage.vue", () => {
-    test("renders correctly", () => {
-        const wrapper = shallowMount(TrainingPage);
-        expect(wrapper.element).toMatchSnapshot();
-    })
+  let wrapper;
+
+  beforeEach(() => {
+    getJobsModule.getJobs = jest.fn();
+      
+    wrapper = shallowMount(TrainingPage, {
+      global: {
+        mocks: {
+          $route: {
+            params: {
+              modelId: "model-id",
+            },
+          },
+          $store: {
+            getters: {
+              getModelById: () => {
+                return {};
+              },
+            },
+          },
+        },
+      },
+    });
+  });
+
+  it("renders", () => {
+    expect(wrapper.exists()).toBe(true);
+  });
 });
--- vue_ui/src/pages/training/services/TrainService.ts
+++ vue_ui/src/pages/training/services/TrainService.ts
@@ -3,7 +3,7 @@
 // Sprint: 3
 // Last modified: 23-05-2023
 
-export async function selectJob(jobId, tableName) {
+export async function selectJob(jobId: string, tableName: string) {
   const response = await fetch("training", {
     method: "POST",
     headers: {
@@ -17,7 +17,7 @@ export async function selectJob(jobId, tableName) {
   return await response.text();
 }
 
-async function checkForRecords(trainingId) {
+async function checkForRecords(trainingId: string) {
   console.log(trainingId);
   const response = await fetch(
     "training/check-records/?trainingId=" + trainingId,
@@ -31,7 +31,7 @@ async function checkForRecords(trainingId) {
   return await response.text();
 }
 
-async function getRecords(trainingId) {
+async function getRecords(trainingId: string) {
   if (await checkForRecords(trainingId)) {
     const response = await fetch(
       "training/records?trainingId=" + trainingId,
@@ -49,7 +49,7 @@ async function getRecords(trainingId) {
   }
 }
 
-export async function getMappedRecords(trainingId) {
+export async function getMappedRecords(trainingId: string) {
   const records = await getRecords(trainingId);
   if (records.records[0] === null) {
     return null;
@@ -70,7 +70,7 @@ export async function getMappedRecords(trainingId) {
   return mappedRecords;
 }
 
-export async function giveAnswer(answer, trainingId) {
+export async function giveAnswer(answer: string, trainingId: string) {
   const response = await fetch("training/give-answer", {
     method: "PUT",
     headers: {
@@ -84,7 +84,7 @@ export async function giveAnswer(answer, trainingId) {
   return await response.json();
 }
 
-export async function saveTraining(modelId, trainingId) {
+export async function saveTraining(modelId: string, trainingId: string) {
   const response = await fetch("training/save", {
       method: "PUT",
       headers: {
