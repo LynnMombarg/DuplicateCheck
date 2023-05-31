65360c5316d04e522c28c26649acf21d803faf14
DDRK <diederik1905@gmail.com>
2023-05-31 14:00:19 +0200

    added test

--- vue_ui/jest.config.js
+++ vue_ui/jest.config.js
@@ -21,4 +21,5 @@ module.exports = {
     ],
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.{js,ts,vue}', '!**/node_modules/**'],
+    coverageReporters: ['json', 'lcov', 'text'],
   };
