d655d31c3ae8ba84e004ff44505cb2f5a8fe7be6
Silke Bertisen <sbertisen@hotmail.nl>
2023-06-01 09:44:14 +0200

    changed import

--- vue_ui/src/pages/overview/components/OverviewModelComponent.vue
+++ vue_ui/src/pages/overview/components/OverviewModelComponent.vue
@@ -43,7 +43,7 @@
 </template>
 
 <script>
-import OverviewDropdownComponent from "vue_ui/src/pages/overview/components/OverviewDropdownComponent.vue";
+import OverviewDropdownComponent from "../../../pages/overview/components/OverviewDropdownComponent.vue";
 
 export default {
   name: "OverviewModelComponent",
