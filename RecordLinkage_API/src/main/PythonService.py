0e4535427e8cdb05453d92c671e5c49d1d2f911c
Lynn <l.mombarg1@student.han.nl>
2023-06-02 11:05:07 +0200

    Update PythonService.py

--- RecordLinkage_API/src/main/PythonService.py
+++ RecordLinkage_API/src/main/PythonService.py
@@ -13,7 +13,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from .RecordLinkageModel import RecordLinkageModel
 import pickle
-
 from main.BlobStorageDAO import BlobStorageDAO
 
 class PythonService:
