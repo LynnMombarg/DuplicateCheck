156c755576f71da7e7ccecb50140fc16b9645e9d
DDRK <diederik1905@gmail.com>
2023-06-02 13:24:06 +0200

    Update OverviewDropdownComponent.spec.js

--- vue_ui/__tests__/pages/overview/components/OverviewDropdownComponent.spec.js
+++ vue_ui/__tests__/pages/overview/components/OverviewDropdownComponent.spec.js
@@ -1,81 +1,78 @@
-// Authors: Lynn
+// Authors: Lynn, Diederik
 // Jira-task: 144
-// Sprint: 3
-// Last modified: 23-05-2023
+// Sprint: 3, 4
+// Last modified: 02-06-2023
 
 import OverviewDropdownComponent from "@/pages/overview/components/OverviewDropdownComponent.vue";
 
 import { mount, shallowMount } from "@vue/test-utils";
-import {
-  createRouter,
-  createWebHashHistory,
-  createWebHistory,
-} from "vue-router";
-// import TrainingPage from "@/pages/training/TrainingPage.vue";
 
 describe("OverviewDropdownComponent.vue", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(OverviewDropdownComponent);
+  });
+
   test("renders", () => {
-    const wrapper = shallowMount(OverviewDropdownComponent);
     expect(wrapper.exists()).toBe(true);
   });
 
   test("dropdown not visible when not clicked", () => {
-    const wrapper = shallowMount(OverviewDropdownComponent);
     expect(wrapper.find("MenuItems").exists()).toBe(false);
   });
 
-  //   test("triggers router push after clicking on train model", async () => {
-  //     const router = createRouter({
-  //       history: createWebHashHistory(),
-  //       routes: [
-  //         {
-  //           path: "/training",
-  //           name: "TrainingPage",
-  //           component: TrainingPage,
-  //         },
-  //       ],
-  //     });
-
-  //     router.push("training");
-  //     await router.isReady();
-
-  //     const wrapper = shallowMount(OverviewDropdownComponent, {
-  //       global: {
-  //         plugins: [router],
-  //       },
-  //     });
-
-  //     expect(wrapper.findComponent(TrainingPage).exists()).toBe(true);
-  //   });
-
-  test("start execution sets values", async () => {
-    const wrapper = shallowMount(OverviewDropdownComponent, {
-        methods: {
-            resetValues: jest.fn(),
-        }
-    });
-
-    wrapper.vm.startExecuteModel("modelId", "tableName");
-
-    expect(wrapper.vm.$data.dialog).toBe(true);
-    expect(wrapper.vm.$data.executeModelId).toBe("modelId");
-    expect(wrapper.vm.$data.executeTableName).toBe("tableName");
-  });
-
   test("execute model while both recordIds are empty", async () => {
-    const wrapper = shallowMount(OverviewDropdownComponent);
-
     wrapper.vm.executeModel();
 
     expect(wrapper.vm.$data.warningVisible).toBe(true);
   });
 
   test("execute model while both recordIds are filled", async () => {
-    const wrapper = shallowMount(OverviewDropdownComponent);
     wrapper.vm.$data.recordId = "1";
     wrapper.vm.$data.modelId = "1";
     wrapper.vm.executeModel();
 
     expect(wrapper.vm.$data.dialog).toBe(false);
+  });
+
+  test("data is loaded", async () => {
+    expect(wrapper.vm.$data.executeTableName).toBe("");
+    expect(wrapper.vm.$data.executeModelId).toBe("");
+    expect(wrapper.vm.$data.recordid1).toBe("");
+    expect(wrapper.vm.$data.recordid2).toBe("");
+    expect(wrapper.vm.$data.warningVisible).toBe(false);
+    expect(wrapper.vm.$data.dialog).toBe(false);
+    expect(wrapper.vm.$data.percentage).toBe(0);
+  });
+
+  test("resetValues", async () => {
+    wrapper.vm.$data.recordid1 = "1";
+    wrapper.vm.$data.executeModelId = "1";
+    wrapper.vm.resetValues();
+
+    expect(wrapper.vm.$data.recordid1).toBe("");
+    expect(wrapper.vm.$data.executeModelId).toBe("");
+  });
+
+  test("confirmDelete", async () => {
+    windowConfirmMock = jest.fn();
+    window.confirm = windowConfirmMock;
+
+    wrapper.vm.confirmDelete();
+
+    expect(windowConfirmMock).toBeCalled();
+  });
+
+  test("defineProps", async () => {
+    const wrapper = mount(OverviewDropdownComponent, {
+      props: {
+        modelId: "1",
+        tableName: "1",
+      },
+    });
+    
+    expect(typeof wrapper.props().modelId).toBe("string");
+    expect(typeof wrapper.props().tableName).toBe("string");
   });
 });
